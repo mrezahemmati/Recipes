d634f5e501fb94d3158dd6abf73fe035d5187563
Reza <mreza.hemmati@outlook.com>
2025-01-26 17:21:24 -0800

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Mohammadreza Hemmati on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
