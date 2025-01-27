03c3871f13047d926dc5c5d4a9269d3d2b3815c1
Reza <mreza.hemmati@outlook.com>
2025-01-26 19:32:03 -0800

    feat: add preview target

--- Packages/Recipes/Package.swift
+++ Packages/Recipes/Package.swift
@@ -10,18 +10,34 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RecipesCore",
-            targets: ["RecipesCore"]),
+            targets: ["RecipesCore"]
+        ),
         .library(
             name: "RecipesUI",
-            targets: ["RecipesUI"]),
+            targets: ["RecipesUI"]
+        ),
+        .library(
+            name: "RecipesUIPreview",
+            targets: ["RecipesUIPreview"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RecipesCore"),
+            name: "RecipesCore"
+        ),
         .target(
-            name: "RecipesUI", dependencies: ["RecipesCore"]),
+            name: "RecipesUI",
+            dependencies: ["RecipesCore"]
+        ),
+        .target(
+            name: "RecipesUIPreview",
+            dependencies: ["RecipesUI"],
+            resources: [
+                .process("Resources")
+            ]
+        ),
         .testTarget(
             name: "RecipesCoreTests",
             dependencies: ["RecipesCore"],
--- Packages/Recipes/Sources/RecipesUI/RecipesView.swift
+++ Packages/Recipes/Sources/RecipesUI/RecipesView.swift
@@ -1,2 +1,19 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  RecipesView.swift
+//  Recipes
+//
+//  Created by Mohammadreza Hemmati on 1/26/25.
+//
+
+import SwiftUI
+import RecipesCore
+
+struct RecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipesView()
+}
